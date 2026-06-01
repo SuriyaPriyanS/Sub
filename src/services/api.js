e0c03397f83f744d7316d27a94efde5aa72e82a3
Suriya Priyan <suriyapriyan0506@gmail.com>
2026-06-01 07:36:20 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://subscripationmanagement-server.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
